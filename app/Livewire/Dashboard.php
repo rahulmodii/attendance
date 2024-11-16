7cf0663ef0e9b7b64c447c145d9b3187d6c16db5
rahulmodii <rmodi2407@gmail.com>
2024-11-16 18:16:05 +0530

    new changes

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -12,25 +12,42 @@ class Dashboard extends Component
 
     public $filterdate;
 
-    public function mount(){
+    public function mount()
+    {
         $this->filterdate = Carbon::now()->format('Y-m-d');
     }
 
-    public function updatedFilterdate($value){
+    public function updatedFilterdate($value)
+    {
         // dd($value);
         $this->filterdate = $value;
     }
 
-
     public function render()
     {
-        $specificDate =  $this->filterdate;
+        $specificDate = $this->filterdate;
         $id = Auth::user()->id;
         $data = User::where('parent_id', $id)->with(['attendanceSessions' => function ($query) use ($specificDate) {
             $query->whereDate('date', $specificDate)
                 ->selectRaw('user_id, MIN(in_time) as first_in_time, MAX(out_time) as last_out_time')
                 ->groupBy('user_id');
         }])->get();
+
+        $newData = User::find($id)->with(['attendanceSessions' => function ($query) use ($specificDate) {
+            $query->whereDate('date', $specificDate)
+                ->selectRaw('user_id, MIN(in_time) as first_in_time, MAX(out_time) as last_out_time')
+                ->groupBy('user_id');
+        }])->first();
+        if ($newData) {
+            $newData['totalMinute'] = $newData->totalTime($this->filterdate);
+            $new = $newData->attendanceSessions->first();
+            $newData['first_in_time'] = 'N/A';
+            $newData['first_out_time'] = 'N/A';
+            if ($new) {
+                $newData['first_in_time'] = $new->first_in_time;
+                $newData['first_out_time'] = $new->last_out_time;
+            }
+        }
 
         foreach ($data as $key => $value) {
             $value['totalMinute'] = $value->totalTime($this->filterdate);
@@ -42,6 +59,6 @@ class Dashboard extends Component
                 $value['first_out_time'] = $new->last_out_time;
             }
         }
-        return view('livewire.dashboard', compact('data'));
+        return view('livewire.dashboard', compact('data','newData'));
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -21,6 +21,92 @@
     <div id="kt_app_content" class="app-content flex-column-fluid">
         <div id="kt_app_content_container" class="app-container container-fluid">
             <div class="row g-5 g-xl-10">
+                {{-- @dd($newData); --}}
+                <div class="col-xl-4 mb-xl-10">
+                    <div class="card card-flush h-xl-100">
+                        <div class="card-header rounded bgi-no-repeat bgi-size-cover bgi-position-y-top bgi-position-x-center align-items-start h-250px"
+                            style="background-image:url('assets/media/svg/shapes/top-green.png"
+                            data-bs-theme="light">
+                            <h3 class="card-title align-items-start flex-column text-white pt-15"> <span
+                                    class="fw-bold fs-2x mb-3">Your Attendance</span>
+                                <div class="fs-4 text-white">
+                                    <span class="">Employee Name: {{ $newData->name ?? '' }}</span><br/>
+                                    <span class="">Date: {{ $filterdate ?? '' }}</span>
+                                </div>
+                            </h3>
+                            <div class="card-toolbar pt-5">
+                                {{-- <button
+                                class="btn btn-sm btn-icon btn-active-color-primary btn-color-white bg-white bg-opacity-25 bg-hover-opacity-100 bg-hover-white bg-active-opacity-25 w-20px h-20px"
+                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end"
+                                data-kt-menu-overflow="true"> <i class="ki-outline ki-dots-square fs-4"></i>
+                            </button> --}}
+                            </div>
+                        </div>
+                        <div class="card-body mt-n20">
+                            <div class="mt-n20 position-relative">
+                                <div class="row g-3 g-lg-6">
+                                    <div class="col-6">
+                                        <div class="bg-gray-100 bg-opacity-70 rounded-2 px-6 py-5">
+                                            <div class="symbol symbol-30px me-5 mb-8"> <span class="symbol-label">
+                                                    <i class="ki-outline ki-flask fs-1 text-primary"></i> </span>
+                                            </div>
+                                            <div class="m-0"> <span
+                                                    class="text-gray-700 fw-bolder d-block fs-2qx lh-1 ls-n1 mb-1">{{ $newData->totalMinute ?? '' }}</span>
+                                                <span class="text-gray-500 fw-semibold fs-6">Total minutes</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="bg-gray-100 bg-opacity-70 rounded-2 px-6 py-5">
+                                            <div class="symbol symbol-30px me-5 mb-8"> <span class="symbol-label">
+                                                    <i class="ki-outline ki-bank fs-1 text-primary"></i> </span>
+                                            </div>
+                                            <div class="m-0">
+                                                <span
+                                                    class="text-gray-700 fw-bolder d-block fs-2qx lh-1 ls-n1 mb-1">
+                                                    @php
+                                                        $timeFormatted = \Carbon\CarbonInterval::minutes($newData->totalMinute)
+                                                            ->cascade()
+                                                            ->forHumans([
+                                                                'short' => true, // Shortens the format, e.g., "2h 5m"
+                                                                'parts' => 2, // Limits to 2 components, e.g., "2h 5m" instead of "2h 5m 0s"
+                                                            ]);
+
+                                                    @endphp
+                                                    {{ $timeFormatted ?? ''}}
+                                                </span>
+                                                <span class="text-gray-500 fw-semibold fs-6">Total time in
+                                                    hours</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="bg-gray-100 bg-opacity-70 rounded-2 px-6 py-5">
+                                            <div class="symbol symbol-30px me-5 mb-8"> <span class="symbol-label">
+                                                    <i class="ki-outline ki-award fs-1 text-primary"></i> </span>
+                                            </div>
+                                            <div class="m-0"> <span
+                                                    class="text-gray-700 fw-bolder d-block fs-2qx lh-1 ls-n1 mb-1">{{ $newData->first_in_time ?? '' }}</span>
+                                                <span class="text-gray-500 fw-semibold fs-6">Check In time</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="bg-gray-100 bg-opacity-70 rounded-2 px-6 py-5">
+                                            <div class="symbol symbol-30px me-5 mb-8"> <span class="symbol-label">
+                                                    <i class="ki-outline ki-timer fs-1 text-primary"></i> </span>
+                                            </div>
+                                            <div class="m-0"> <span
+                                                    class="text-gray-700 fw-bolder d-block fs-2qx lh-1 ls-n1 mb-1">{{ $newData->first_out_time ?? '' }}</span>
+                                                <span class="text-gray-500 fw-semibold fs-6">Check out time</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 @foreach ($data as $value)
                     <div class="col-xl-4 mb-xl-10">
                         <div class="card card-flush h-xl-100">
