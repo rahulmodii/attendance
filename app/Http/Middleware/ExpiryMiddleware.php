b709aa112e5aa28a193edaa50fe0ce4157d7d63e
rahulmodii <rmodi2407@gmail.com>
2024-12-04 15:04:58 +0530

    new changes

--- app/Http/Middleware/ExpiryMiddleware.php
+++ app/Http/Middleware/ExpiryMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
@@ -16,7 +17,14 @@ class ExpiryMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $date = auth()->user()->expiry_date;
+
+        if (auth()->user()->role == 1) {
+            $date = auth()->user()->expiry_date;
+        }
+        if (auth()->user()->role == 2) {
+            $parentId = auth()->user()->parent_id;
+            $date = User::find($parentId)->expiry_date;
+        }
 
         if (Carbon::parse($date)->isPast()) {
             return redirect()->route('packages');
