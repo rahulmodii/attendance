c705bb4554193f5845dce03ff3e7f0023d3e9abd
rahulmodii <rmodi2407@gmail.com>
2024-12-16 19:41:11 +0530

    new changes

--- resources/views/livewire/white-label.blade.php
+++ resources/views/livewire/white-label.blade.php
@@ -10,8 +10,8 @@
                 <!--begin::Col-->
                 <div class="col-xl-12">
                     <h4 class="text-gray-800 mb-0">Domain</h4>
-                    <p class="fs-6 fw-semibold text-gray-600 py-4 m-0">Plan your blog post by choosing a topic, creating an outline conduct
-                    <br>research, and checking facts</p>
+                    <p class="fs-6 fw-semibold text-gray-600 py-4 m-0">Add your A record in domain
+                    <br>@104.237.9.112</p>
                     <div class="d-flex">
                         <input  wire:model='domain' type="text" class="form-control form-control-solid me-3 flex-grow-1" >
                         <button  class="btn btn-primary btn-active-light-primary fw-bold flex-shrink-0" wire:click='saveDomain'>Save</button>
